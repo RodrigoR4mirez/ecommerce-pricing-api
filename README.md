72c054c31dbc04f01efd7a1b36a18cc91945e749
RodrigoR <r4mirez.r@gmail.com>
2025-06-15 13:07:46 -0500

    Initialize Spring Boot project with Maven, including base configuration and dependencies. Added `.gitignore`, `.gitattributes`, `README.md`, and Maven wrapper scripts.

---
+++ README.md
@@ -0,0 +1,11 @@
+# ecommerce-pricing-api
+
+Servicio REST en Spring Boot para consultar el precio de un producto según fecha de aplicación.
+
+El precio de un producto se consulta mediante una petición GET a la ruta:
+
+## Ejecución
+
+```bash
+./mvnw spring-boot:run
+```
---
+++ src/main/java/com/inditex/pricing/ecommercepricingapi/EcommercePricingApiApplication.java
@@ -0,0 +1,22 @@
+package com.inditex.pricing.ecommercepricingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Punto de entrada de la aplicación Spring Boot.
+ */
+@SpringBootApplication
+public class EcommercePricingApiApplication {
+
+    /**
+     * Método principal que inicia la aplicación.
+     *
+     * @param args argumentos de la línea de comandos
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(EcommercePricingApiApplication.class, args);
+
+    }
+
+}
